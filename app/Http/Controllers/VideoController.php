e454c0d3c2d4ed5add6f38c639d6626a061774a2
Mario Laserna <laserna.mario@gmail.com>
2017-02-08 15:19:43 -0500

    cambio en ruta de ffmpeg

--- app/Http/Controllers/VideoController.php
+++ app/Http/Controllers/VideoController.php
@@ -47,8 +47,8 @@ class VideoController extends Controller
         //$video_url = storage_path('app/videos') . '/webm.webm';
 
         $ffmpeg = FFMpeg::create([
-            'ffmpeg.binaries'  => 'C:/ffmpeg/bin/ffmpeg.exe',
-            'ffprobe.binaries' => 'C:/ffmpeg/bin/ffprobe.exe',
+            /*'ffmpeg.binaries'  => 'C:/ffmpeg/bin/ffmpeg.exe',
+            'ffprobe.binaries' => 'C:/ffmpeg/bin/ffprobe.exe',*/
             'timeout'          => 0, // The timeout for the underlying process
             'ffmpeg.threads'   => 12,   // The number of threads that FFMpeg should use
         ]);
